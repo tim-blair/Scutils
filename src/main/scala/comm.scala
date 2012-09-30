d3a677974f28429aeaa2993955ca5a26a18b4093
Tim Blair <trblair@gmail.com>
2012-09-29 21:36:27 -0700

    Some argument parsing

--- src/main/scala/comm.scala
+++ src/main/scala/comm.scala
@@ -20,31 +20,37 @@ object Comm extends App {
   
   
   override def main(args: Array[String]) {
-    val first = readFile(args(0))
-    val second = readFile(args(1))
+    val fileNames = args.takeRight(2)
+    val arguments = args.dropRight(2)
+    val first = readFile(fileNames(0))
+    val second = readFile(fileNames(1))
     
-    val output = process(first, second)
-    output.foreach(println)
+    val output = process(first, second, arguments)
+    output.foreach(str => str match {
+      case None =>
+      case Some(string) => println(string)
+    })
   }
   
-  def process(first: List[String], second: List[String]): List[String] = {
-    if(first.isEmpty && second.isEmpty)
+  def process(first: List[String], second: List[String], args: Array[String]): List[Option[String]] = {
+    def firstColumn(str: String): Option[String] = if(args.contains("-1")) None else Some(str)
+    def secondColumn(str: String): Option[String] = if(args.contains("-2")) None else Some("\t" + str)
+    def thirdColumn(str: String): Option[String] = if(args.contains("-3")) None else Some("\t\t" + str)
+
+	if(first.isEmpty && second.isEmpty)
       Nil
     else if(first.isEmpty)
-      secondColumn(second.head) :: process(first, second.tail)
+      secondColumn(second.head) :: process(first, second.tail, args)
     else if(second.isEmpty)
-      firstColumn(first.head) :: process(first.tail, second)
+      firstColumn(first.head) :: process(first.tail, second, args)
     else if(first.head == second.head)
-      thirdColumn(first.head) :: process(first.tail, second.tail)
+      thirdColumn(first.head) :: process(first.tail, second.tail, args)
     else if(first.head > second.head)
-      secondColumn(second.head) :: process(first, second.tail)
+      secondColumn(second.head) :: process(first, second.tail, args)
     else // first.head < second.head
-      firstColumn(first.head) :: process(first.tail, second)
+      firstColumn(first.head) :: process(first.tail, second, args)
   }
   
-  def firstColumn(str: String): String = str
-  def secondColumn(str: String): String = "\t" + str
-  def thirdColumn(str: String): String = "\t\t" + str
 
     def readFile(file: String): List[String] = {
     Source.fromFile(new File(file)).getLines.toList

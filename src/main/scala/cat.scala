219decab6a15b4f3fe563d1f7ffae6bbc4599ba1
Tim Blair <trblair@gmail.com>
2012-09-30 17:10:57 -0700

    Reformat cat to make scalaide happy

--- src/main/scala/cat.scala
+++ src/main/scala/cat.scala
@@ -12,45 +12,48 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+object Cat extends App {
+  override def main(args: Array[String]) = {
+    def checkArg(str: String) =
+      args.length > 0 && args.contains(str)
 
-//-n means number each line
-val numbered = checkArg("-n")
-//-b means number non-empty lines, overrides -n
-val nonBlankNum = checkArg("-b")
-//-E display $ at end of each line
-val showEnds = checkArg("-E")
-//-s suppress repeated empty lines
-val suppress = checkArg("-s")
+    //-n means number each line
+    val numbered = checkArg("-n")
+    //-b means number non-empty lines, overrides -n
+    val nonBlankNum = checkArg("-b")
+    //-E display $ at end of each line
+    val showEnds = checkArg("-E")
+    //-s suppress repeated empty lines
+    val suppress = checkArg("-s")
 
-val srcs = 
-	if(args.length > 0 && args.contains("-"))
-		io.Source.stdin :: Nil
-	else
-		args filter(arg => !arg.startsWith("-")) map(io.Source.fromFile(_)) toList
-var num = 1
-var prevWasBlank = false
-srcs.foreach(src => {
-	src.getLines.foreach(line => {
-		if(!(suppress && prevWasBlank && line == "")) {
-			prevWasBlank = line == ""
-			if(nonBlankNum) {
-				//Re-use the check we already did
-				if(prevWasBlank) {
-					printf("%6d ", num)
-					num += 1
-				}
-			} else if(numbered) {
-				//real cat pads and puts a space -- assumes under 1 million lines?
-				printf("%6d ", num)
-				num += 1
-			}
-			print(line)
-			if(showEnds)
-				print("$")
-			println()
-		}
-	})
-})
-
-def checkArg(str: String) =
-	args.length > 0 && args.contains(str)
+    val srcs = 
+      if(args.length > 0 && args.contains("-"))
+		    io.Source.stdin :: Nil
+	    else
+		    args filter(arg => !arg.startsWith("-")) map(io.Source.fromFile(_)) toList
+    var num = 1
+    var prevWasBlank = false
+    srcs.foreach(src => {
+      src.getLines.foreach(line => {
+        if(!(suppress && prevWasBlank && line == "")) {
+          prevWasBlank = line == ""
+          if(nonBlankNum) {
+				    // Re-use the check we already did
+				    if(prevWasBlank) {
+					    printf("%6d ", num)
+					    num += 1
+				    }
+			    } else if(numbered) {
+				    // real cat pads and puts a space -- assumes under 1 million lines?
+				    printf("%6d ", num)
+				    num += 1
+			    }
+			    print(line)
+			    if(showEnds)
+				    print("$")
+			    println()
+		    }
+	    })
+    })
+  }
+}
